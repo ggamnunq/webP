5e8d6cb92bc5fb751720e23c1c9a8a683ba026f9
Junyong <93406666+ggamnunq@users.noreply.github.com>
2024-12-06 02:38:10 +0900

    Add files via upload

---
+++ app10.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const static = require('serve-static');
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/', static(path.join(__dirname, 'public')));
+
+router.route('/process/login').post((req, res) => {
+    console.log('/process/login 처리');
+
+    const paramId = req.body.id || req.query.id;
+    const paramPassword = req.body.password || req.query.password;
+
+
+    res.status(200).send(
+        `<h1>서버에 응답한 결과</h1.
+        <div><p>Param id : ${paramId}</p></div>
+        <div><p>Param password : ${paramPassword}</p></div>
+        <br><br><a href="/login3.html">로그인 페이지로 돌아가기</a>
+        `        
+    );
+});
+
+app.use('/', router);
+
+app.listen(3000);
